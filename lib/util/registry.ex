961648a3fe37326a049dfdcd5c93e8cbfe0f8662
bigmeech <denachural@gmail.com>
2018-07-17 08:16:18 +0100

    rename WatchdogAgent to just agent, add watchdog registry

---
+++ lib/util/registry.ex
@@ -0,0 +1,17 @@
+defmodule Watchdog.Util.Registry do
+    def register_name do
+        
+    end
+
+    def unregister_name do
+        
+    end
+
+    def whereis_name do
+        
+    end
+
+    def send do
+        
+    end
+end
